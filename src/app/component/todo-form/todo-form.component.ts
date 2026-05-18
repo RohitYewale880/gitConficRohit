aec57b5a9dcbc2467b86ea067a5bffb7dfbaa02f
arjunchavan2323 <arjunchavan1791@gmail.com>
2026-05-18 21:30:58 +0530

    edit-update work done

--- src/app/component/todo-form/todo-form.component.ts
+++ src/app/component/todo-form/todo-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Itodo } from 'src/app/modals/todo';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
   styleUrls: ['./todo-form.component.scss']
 })
-export class TodoFormComponent implements OnInit {
-
+export class TodoFormComponent implements OnInit, OnChanges {
+  isIneditmode :boolean=false
+@Input()todoeditobj!:Itodo
+@Output() emitupdate:EventEmitter <Itodo>=new EventEmitter <Itodo> ()
+@ViewChild('TodoItem') TodoItem !:ElementRef
   constructor() { }
 
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+   if(changes['todoeditobj'].currentValue){
+    this.isIneditmode=true
+    this.TodoItem.nativeElement.value=this.todoeditobj.todoItem
+
+   }
+  }
+
   ngOnInit(): void {
+  }
+
+  onupdatebtn(){
+    let new_updateobj:Itodo={
+      todoItem:this.TodoItem.nativeElement.value,
+      todoId:this.todoeditobj.todoId
+    }
+    this.isIneditmode=false
+    this.emitupdate.emit(new_updateobj)
+
   }
 
 }
--- src/app/component/todo-list/todo-list.component.ts
+++ src/app/component/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Itodo } from 'src/app/modals/todo';
 
 @Component({
@@ -9,12 +9,18 @@ import { Itodo } from 'src/app/modals/todo';
 export class TodoListComponent implements OnInit {
 
   constructor() { }
+  @Output() emitedittodo :EventEmitter<Itodo>=new EventEmitter<Itodo>()
   @Input() todosarray! : Array<Itodo>
   ngOnInit(): void {
   }
 
   trackByfun(index : number, item:Itodo){
     return item.todoId
+  }
+
+  onedittodo(edittodo:Itodo){
+    this.emitedittodo.emit(edittodo)
+
   }
 
 }
