c4e6ec199c9ba1c93e3833ff46bd446a8f5e6bc3
RohitYewale880 <rohityewale2020@gmail.com>
2026-05-18 20:50:42 +0530

    read fun done by rohit

---
+++ src/app/component/todo-dashboard/todo-dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { todoArr } from 'src/app/consts/todo';
+import { Itodo } from 'src/app/modals/todo';
+
+@Component({
+  selector: 'app-todo-dashboard',
+  templateUrl: './todo-dashboard.component.html',
+  styleUrls: ['./todo-dashboard.component.scss']
+})
+export class TodoDashboardComponent implements OnInit {
+  array!: Array<Itodo>
+  constructor() { }
+
+  ngOnInit(): void {
+    this.array = todoArr
+  }
+
+}
