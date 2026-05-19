c8d6736b60ea618b6a2fef3b23dffd9bd6017d51
shivrajpathadedev-dev <shivrajpathade.dev@gmail.com>
2026-05-19 11:22:01 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { TodoDashboardComponent } from './component/todo-dashboard/todo-dashboard.component';
 import { TodoFormComponent } from './component/todo-form/todo-form.component';
 import { TodoListComponent } from './component/todo-list/todo-list.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import { TodoListComponent } from './component/todo-list/todo-list.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
